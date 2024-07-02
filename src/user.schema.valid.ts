82985d87a52a9300702084ddf279995904f206cb
RAMIARANJAFY Tsitohaina <ramiaranjafy@kali>
2024-07-02 18:53:07 +0200

    name required et id required

--- src/user.schema.valid.ts
+++ src/user.schema.valid.ts
@@ -1,14 +1,24 @@
 // importation de 'yup' pour la validation 
 import * as Yup from 'yup'
 
+const MINIMUM_LENGTH = {
+    name: 4,
+    city : 4
+}
+
+const MAXIMUM_LENGTH = {
+    name : 20,
+    city : 30
+}
+
 export const addUserSchemaValidator = {
     schema: {
         // validation pour le body qu'on vient de reçeiver depuis req.body
         body: {
             yupSchema: Yup.object().shape({
-                name : Yup.string().required(),
+                name : Yup.string().min(MINIMUM_LENGTH.name).max(MAXIMUM_LENGTH.name).required(),
                 email : Yup.string().email().required(),
-                city : Yup.string(),
+                city : Yup.string().min(MINIMUM_LENGTH.city).max(MAXIMUM_LENGTH.city),
                 genre : Yup.string().oneOf(["femele","male"]).required()    
             })
         }
@@ -17,17 +27,33 @@ export const addUserSchemaValidator = {
 
 export const updateUserSchemaValidator = {
     schema: {
-        // validation pour le body qu'on vient de reçeiver depuis req.body
+        // validation pour le body qu'on vient de reçeiver depuis req.body et le paramètre
         body: {
             yupSchema: Yup.object().shape({
-                name : Yup.string().required(),
+                name : Yup.string().min(MINIMUM_LENGTH.name).max(MAXIMUM_LENGTH.name).required(),
                 email : Yup.string().email().required(),
-                city : Yup.string(),
+                city : Yup.string().min(MINIMUM_LENGTH.city).max(MAXIMUM_LENGTH.city),
                 genre : Yup.string().oneOf(["femele","male"]).required()    
+            })
+        },
+        params: {
+            yupSchema: Yup.object().shape({
+              id : Yup.number().integer().positive().required()
             })
         }
       }
 }
+export const afficheUserSchemaValidator = {
+    schema: {
+        // validation pour le paramètre id
+        params: {
+            yupSchema: Yup.object().shape({
+              id : Yup.number().integer().positive().required()
+            })
+        }
+      }
+}
+
 
 /*let userSchema = object({
     name: string().required(),
